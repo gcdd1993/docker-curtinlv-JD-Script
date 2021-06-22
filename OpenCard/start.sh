a6255d1c977c3618d10b687c65c5b038c324156a
gcdd1993 <gcwm99@gmail.com>
2021-06-22 21:03:04 +0800

    openCardBean 从容器环境变量获取

--- OpenCard/start.sh
+++ OpenCard/start.sh
@@ -11,9 +11,10 @@
 scriptPath='jd_OpenCard.py'
 ################### ↓↓↓【以下需要配置的参数，代替配置文件OpenCardConfig.ini】↓↓↓ ###################
 #京东cookie 格式：pt_key=xxx;pt_pin=xxx; & pt_key=xxx;pt_pin=xxx; (多账号&分隔)
+#export JD_COOKIE='你的京东Cookie放这里，单引号保留'
 
 #只入送豆数量大于此值
-export openCardBean=5
+#export openCardBean=5
 
 #False|True 是否记录符合条件的shopid，输出文件【log/shopid-yyyy-mm-dd.txt】
 export record=True
