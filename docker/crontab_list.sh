0ac019eb20d611503f6d32b099baed1102eb8b44
gcdd1993 <gcwm99@gmail.com>
2021-06-26 23:10:57 +0800

    fix

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -4,3 +4,7 @@
 0 8,15 * * * sh /scripts/OpenCard/start.sh
 # 关注领豆
 30 8,15 * * * sh /scripts/OpenCard/start.sh
+# 东东超市商品兑换
+0 0 * * * sh /scripts/BlueCoin/start.sh
+# 微信小程序-赚京豆-瓜分10亿京豆自动助力，默认给账号1助力，多账号才能玩~
+0 8 * * * sh /scripts/zjd/start.sh
