dcdbac2d30665044bb81be71044bac2a28332240
gcdd1993 <gcwm99@gmail.com>
2021-07-03 18:06:45 +0800

    fix

--- BlueCoin/jd_blueCoin.py
+++ BlueCoin/jd_blueCoin.py
@@ -1,5 +1,15 @@
 #!/bin/env python3
 # -*- coding: utf-8 -*
+
+import datetime
+import os
+import threading
+import time
+from urllib.parse import unquote
+
+import re
+import requests
+
 '''
 项目名称: test / jd_blueCoin
 Author: Curtin
@@ -16,45 +26,40 @@ Date: 2021/4/17 上午11:22
 2021-04-26：
 * 增加多线程并发
 '''
-################【参数】######################
-#【填写您要兑换的商品】
-coinToBeans='京豆包'
-#轮次
-startMaxNum=30
-#多线程并发，相当于每秒点击兑换次数...适当调整，手机会发烫
-dd_thread=30
+# 【填写您要兑换的商品】
+coinToBeans = '京豆包'
+# 轮次
+startMaxNum = 30
+# 多线程并发，相当于每秒点击兑换次数...适当调整，手机会发烫
+dd_thread = 30
 
-#开始抢兑时间
-starttime='23:59:59.00000000'
-#结束时间
-endtime='00:00:30.00000000'
+# 开始抢兑时间
+starttime = '23:59:59.00000000'
+# 结束时间
+endtime = '00:00:30.00000000'
 
-###############################################
-import time,os,datetime
-import requests,re
-from urllib.parse import quote, unquote
-import threading
-import urllib3
-#urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+# urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 requests.packages.urllib3.disable_warnings()
 
-timestamp=int(round(time.time() * 1000))
-script_name='东东超市商品兑换-Python'
-title=''
-prizeId=''
-blueCost=''
-inStock=''
-#您的ck格式：pt_key=xxx;pt_pin=xxx; (多账号&分隔)
-cookies=os.environ["JD_COOKIE"]
+timestamp = int(round(time.time() * 1000))
+script_name = '东东超市商品兑换-Python'
+title = ''
+prizeId = ''
+blueCost = ''
+inStock = ''
+# 您的ck格式：pt_key=xxx;pt_pin=xxx; (多账号&分隔)
+cookies = os.environ["JD_COOKIE"]
 
 today = datetime.datetime.now().strftime('%Y-%m-%d')
-tomorrow=(datetime.datetime.now() + datetime.timedelta(days=1)).strftime('%Y-%m-%d')
+tomorrow = (datetime.datetime.now() + datetime.timedelta(days=1)).strftime('%Y-%m-%d')
+
 
 class TaskThread(threading.Thread):
     """
     处理task相关的线程类
     """
+
     def __init__(self, func, args=()):
         super(TaskThread, self).__init__()
         self.func = func  # 要执行的task类型
@@ -76,11 +81,11 @@ class TaskThread(threading.Thread):
 
 def iscookie():
     if 'pt_key=' in cookies and 'pt_pin=' in cookies:
-        r = re.compile(r"pt_key=.*?pt_pin=.*?;" ,  re.M | re.S | re.I)
+        r = re.compile(r"pt_key=.*?pt_pin=.*?;", re.M | re.S | re.I)
         result = r.findall(cookies)
         if len(result) == 1:
             return result[0]
-        elif len(result)>1:
+        elif len(result) > 1:
             return result
         else:
             print("cookie 格式错误！...本次操作已退出")
@@ -89,58 +94,72 @@ def iscookie():
         print("cookie 格式错误！...本次操作已退出")
         exit(9)
 
-def setHeaders(cookie):
+
+def set_headers(cookie):
     try:
         r = re.compile(r"pt_pin=(.*?);")
         userName = r.findall(cookie)
         userName = unquote(userName[0])
     except Exception as e:
-        print(e,"cookie格式有误！")
+        print(e, "cookie格式有误！")
         exit(2)
     headers = {
         'Origin': 'https://jdsupermarket.jd.com',
         'Cookie': cookie,
         'Connection': 'keep-alive',
         'Accept': 'application/json, text/plain, */*',
-        'Referer': 'https://jdsupermarket.jd.com/game/?tt={}'.format(timestamp-314),
+        'Referer': 'https://jdsupermarket.jd.com/game/?tt={}'.format(timestamp - 314),
         'Host': 'api.m.jd.com',
-        'User-Agent': 'jdapp;iPhone;9.4.8;14.3;809409cbd5bb8a0fa8fff41378c1afe91b8075ad;network/wifi;ADID/201EDE7F-5111-49E8-9F0D-CCF9677CD6FE;supportApplePay/0;hasUPPay/0;hasOCPay/0;model/iPhone13,4;addressid/2455696156;supportBestPay/0;appBuild/167629;jdSupportDarkMode/0;Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1',
+        'User-Agent': 'jdapp;iPhone;9.4.8;14.3;809409cbd5bb8a0fa8fff41378c1afe91b8075ad;network/wifi;ADID/201EDE7F'
+                      '-5111-49E8-9F0D-CCF9677CD6FE;supportApplePay/0;hasUPPay/0;hasOCPay/0;model/iPhone13,'
+                      '4;addressid/2455696156;supportBestPay/0;appBuild/167629;jdSupportDarkMode/0;Mozilla/5.0 ('
+                      'iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) '
+                      'Mobile/15E148;supportJDSHWK/1',
         'Accept-Encoding': 'gzip, deflate, br',
         'Accept-Language': 'zh-cn'
     }
-    return headers,userName
+    return headers, userName
 
-#查询东东超市蓝币数量
+
+# 查询东东超市蓝币数量
 def getBlueCoinInfo(headers):
     try:
-        url='https://api.m.jd.com/api?appid=jdsupermarket&functionId=smtg_newHome&clientVersion=8.0.0&client=m&body=%7B%22channel%22:%2218%22%7D&t={0}'.format(timestamp)
+        url = 'https://api.m.jd.com/api?appid=jdsupermarket&functionId=smtg_newHome&clientVersion=8.0.0&client=m&body=%7B%22channel%22:%2218%22%7D&t={0}'.format(
+            timestamp)
         respon = requests.get(url=url, verify=False, headers=headers)
         result = respon.json()
-        totalBlue=result['data']['result']['totalBlue']
-        shopName=result['data']['result']['shopName']
-        return totalBlue,shopName
+        totalBlue = result['data']['result']['totalBlue']
+        shopName = result['data']['result']['shopName']
+        return totalBlue, shopName
     except Exception as e:
         print(e)
 
-#查询所有用户蓝币、等级
+
+# 查询所有用户蓝币、等级
 def getAllUserInfo():
     id_num = 1
     for ck in cookies:
-        headers,userName = setHeaders(ck)
+        headers, userName = set_headers(ck)
         try:
-            totalBlue,shopName = getBlueCoinInfo(headers)
-            url = 'https://api.m.jd.com/api?appid=jdsupermarket&functionId=smtg_receiveCoin&clientVersion=8.0.0&client=m&body=%7B%22type%22:4,%22channel%22:%2218%22%7D&t={0}'.format(timestamp)
-            respon = requests.get(url=url, verify=False,  headers=headers)
+            totalBlue, shopName = getBlueCoinInfo(headers)
+            url = 'https://api.m.jd.com/api?appid=jdsupermarket&functionId=smtg_receiveCoin&clientVersion=8.0.0&client=m&body=%7B%22type%22:4,%22channel%22:%2218%22%7D&t={0}'.format(
+                timestamp)
+            respon = requests.get(url=url, verify=False, headers=headers)
             result = respon.json()
             level = result['data']['result']['level']
-            print("【用户{4}:{5}】: {0} {3}\n【等级】: {1}\n【蓝币】: {2}万\n------------------".format(shopName, level, totalBlue / 10000,totalBlue, id_num,userName))
+            print("【用户{4}:{5}】: {0} {3}\n【等级】: {1}\n【蓝币】: {2}万\n------------------".format(shopName, level,
+                                                                                           totalBlue / 10000, totalBlue,
+                                                                                           id_num, userName))
         except Exception as e:
             # print(e)
             print(f"账号{id_num}【{userName}】异常请检查ck是否正常~")
         id_num += 1
-#查询商品
+
+
+# 查询商品
 def smtg_queryPrize(headers, coinToBeans):
-    url = 'https://api.m.jd.com/api?appid=jdsupermarket&functionId=smt_queryPrizeAreas&clientVersion=8.0.0&client=m&body=%7B%22channel%22:%2218%22%7D&t={}'.format(timestamp)
+    url = 'https://api.m.jd.com/api?appid=jdsupermarket&functionId=smt_queryPrizeAreas&clientVersion=8.0.0&client=m&body=%7B%22channel%22:%2218%22%7D&t={}'.format(
+        timestamp)
     try:
         respone = requests.get(url=url, verify=False, headers=headers)
         result = respone.json()
@@ -163,26 +182,29 @@ def smtg_queryPrize(headers, coinToBeans):
         print(e)
 
 
-#判断设置的商品是否存在 存在则返回 商品标题、prizeId、蓝币价格、是否有货
-def isCoinToBeans(coinToBeans,headers):
+# 判断设置的商品是否存在 存在则返回 商品标题、prizeId、蓝币价格、是否有货
+def isCoinToBeans(coinToBeans, headers):
     global title, prizeId
     if coinToBeans.strip() != '':
         try:
-            title, prizeId, blueCost, inStock, skuId = smtg_queryPrize(headers,coinToBeans)
+            title, prizeId, blueCost, inStock, skuId = smtg_queryPrize(headers, coinToBeans)
             return title, prizeId, blueCost, inStock, skuId
         except Exception as e:
             print(e)
             pass
     else:
         print("1.请检查设置的兑换商品名称是否正确?")
-#抢兑换
-def smtg_obtainPrize(prizeId,headers):
+
+
+# 抢兑换
+def smtg_obtainPrize(prizeId, headers):
     timestamp = int(round(time.time() * 1000))
-    url = 'https://api.m.jd.com/api?appid=jdsupermarket&functionId=smtg_obtainPrize&clientVersion=8.0.0&client=m&body=%7B%22prizeId%22:%22{0}%22,%22channel%22:%221%22%7D&t={1}'.format(prizeId, timestamp)
+    url = 'https://api.m.jd.com/api?appid=jdsupermarket&functionId=smtg_obtainPrize&clientVersion=8.0.0&client=m&body=%7B%22prizeId%22:%22{0}%22,%22channel%22:%221%22%7D&t={1}'.format(
+        prizeId, timestamp)
     try:
         respon = requests.post(url=url, verify=False, headers=headers)
-        result=respon.json()
-        success=result['data']['success']
+        result = respon.json()
+        success = result['data']['success']
         bizMsg = result['data']['bizMsg']
         if success == True:
             print(result)
@@ -195,16 +217,15 @@ def smtg_obtainPrize(prizeId,headers):
         print(e)
 
 
-def issmtg_obtainPrize(headers,userName,user_num,maxNum):
-
+def issmtg_obtainPrize(headers, userName, user_num, maxNum):
     try:
-        maxNum=int(maxNum)
-        title, prizeId, blueCost, inStock, skuId = isCoinToBeans(coinToBeans,headers)
+        maxNum = int(maxNum)
+        title, prizeId, blueCost, inStock, skuId = isCoinToBeans(coinToBeans, headers)
         totalBlue, shopName = getBlueCoinInfo(headers)
         if totalBlue > blueCost:
             qgtime = '{} {}'.format(today, starttime)
             qgendtime = '{} {}'.format(tomorrow, endtime)
-            num=1
+            num = 1
             nowtime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f8')
             print(f"当前时间[{nowtime}]\n开始时间[{qgtime}]\n正在等待，请勿终止退出...")
             while True:
@@ -236,25 +257,26 @@ def issmtg_obtainPrize(headers,userName,user_num,maxNum):
                     pass
 
         else:
-            print("[{3}]:你的蓝币{0}w不足够兑换【{1}】, 需要{2}w".format(totalBlue/10000,title,blueCost/10000,userName))
+            print("[{3}]:你的蓝币{0}w不足够兑换【{1}】, 需要{2}w".format(totalBlue / 10000, title, blueCost / 10000, userName))
     except Exception as e:
         print(e)
 
-def checkUser(cookies): #返回符合条件的ck list
-    if isinstance(cookies,list):
+
+def checkUser(cookies):  # 返回符合条件的ck list
+    if isinstance(cookies, list):
         pass
-    elif isinstance(cookies,str):
-        cookies = [cookies,]
+    elif isinstance(cookies, str):
+        cookies = [cookies, ]
     else:
         print("cookie 类型有误")
         exit(2)
-    cookieList=[]
-    user_num=1
+    cookieList = []
+    user_num = 1
     for i in cookies:
-        headers,userName = setHeaders(i)
+        headers, userName = set_headers(i)
         try:
             totalBlue, shopName = getBlueCoinInfo(headers)
-            title,prizeId,blueCost,inStock, skuId = isCoinToBeans(coinToBeans,headers)
+            title, prizeId, blueCost, inStock, skuId = isCoinToBeans(coinToBeans, headers)
             totalBlueW = totalBlue / 10000
             if user_num == 1:
                 print("您已设置兑换的商品：【{0}】 需要{1}w蓝币".format(title, blueCost / 10000))
@@ -266,32 +288,34 @@ def checkUser(cookies): #返回符合条件的ck list
                 print(f"账号{user_num}:【{userName}】蓝币:{totalBlueW}万...no")
         except Exception as e:
             print(f"账号{user_num}:【{userName}】，该用户异常，查不到商品关键词【{coinToBeans}】")
-        user_num+=1
+        user_num += 1
 
-    if len(cookieList) >0:
+    if len(cookieList) > 0:
         print("共有{0}个账号符合兑换条件".format(len(cookieList)))
         return cookieList
     else:
         print("共有{0}个账号符合兑换条件...已退出，请继续加油赚够钱再来~".format(len(cookieList)))
         exit(0)
 
-#Start
+
+# Start
 def start():
     print(f"###### 启动并发线程 【Thread-{dd_thread}】")
     cookies = iscookie()
     cookies = checkUser(cookies)
-    run_num=1
+    run_num = 1
     while True:
         print(f"\n【准备开始第{run_num} 轮...】\n")
-        user_num=1
+        user_num = 1
         for i in cookies:
-            headers,userName = setHeaders(i)
+            headers, userName = set_headers(i)
             issmtg_obtainPrize(headers, userName, user_num, maxNum=0)
-            user_num+=1
+            user_num += 1
         if run_num > startMaxNum:
             print("已结束")
             break
-        run_num+=1
+        run_num += 1
+
 
 if __name__ == '__main__':
     print("【{}】".format(script_name))
---
+++ qjd/start.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+## 使用Env环境执行方式
+## 执行命令 ：sh start.sh
+## 2021.6.6
+## By Curtin
+## 定时任务：
+## 0 8 * * * sh /你存放脚本的本地绝对路径/start.sh
+##
+#########################################
+#主脚本路径。默认和主脚本同级目录
+cd `dirname $0`
+curpwd=`pwd`
+scriptPath="${curpwd}/jd_qjd.py"
+################### ↓↓↓【以下需要配置的参数，代替配置文件OpenCradConfig.ini】↓↓↓ ###################
+#京东cookie 格式：pt_key=xxx;pt_pin=xxx; & pt_key=xxx;pt_pin=xxx; (多账号&分隔)
+
+############【通知参数】############
+####### TG 机器人 #######
+# TG token
+export TG_BOT_TOKEN=
+# UserId
+export TG_USER_ID=
+
+##### 如果你的网络能正常打开TG 以下参数不用配置 ↓↓↓
+# TG_API_HOST
+export TG_API_HOST=
+# TG代理ip 和端口
+export TG_PROXY_IP=
+# TG代理端口
+export TG_PROXY_PORT=
+############################### ↑↑↑
+######## 【微信 推送加】#####
+# token
+export PUSH_PLUS_TOKEN=
+#Bark 推送
+export BARK=
+##################
+
+################### ↑↑↑↑ 你需要填的参数到此结束 ↑↑↑↑ ##############################
+######### 以下不用配置，默认就好 ##########################
+
+_printTime(){
+  echo "[`date +"%F %T"`]: $1"
+}
+
+##############################3
+logfile=${BASH_SOURCE%/*}/run_jd_qjd.log
+
+
+PID=`ps -ef | grep "python3 ${scriptPath}" | grep -v grep| awk '{print $2}'`
+if [ ! -z $PID ];then
+        _printTime "已在后台运行 PID：$PID 如需要终止执行命令：kill $PID"
+        _printTime "查看运行日志: tail -f ${logfile}"
+else
+        _printTime "开始执行抢京豆...."
+        echo "" >${logfile} #清空日志
+        nohup python3 ${scriptPath} >> ${logfile} 2>&1 &
+        sleep 5
+        PID=`ps -ef | grep "python3 ${scriptPath}" | grep -v grep| awk '{print $2}'`
+        if [ -z ${PID} ];then
+                _printTime "执行失败!"
+                _printTime "请检查日志: tail -f ${logfile}"
+        else
+                _printTime "执行成功，已放在后台运行 PID：$PID"
+                _printTime "查看运行日志: tail -f ${logfile}"
+        fi
+fi
